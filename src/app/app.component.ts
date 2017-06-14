8c5c830c56d4c395dedba990d8b51111721e90cb
ant <antdjones@gmail.com>
2017-06-14 07:17:42 -0400

    Altered for presentation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,7 +21,7 @@ import { HealthPartnerPage } from '../pages/health-partner/health-partner';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = SetAvailabilityPage;
+  rootPage: any = HomePage;
 
 
   // rootPage: any = ProgressPage;
